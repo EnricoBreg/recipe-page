18027326d669508ed65601f87a6f113108ca029e
Enrico Bregoli <55693007+EnricoBreg@users.noreply.github.com>
2024-04-29 14:20:56 +0200

    Add files via upload
    Primo commit di setup progetto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "Arial", "Helvetica", "sans-serif"],
+      serif: ["Young-Serif", 'Times New Roman', 'Times', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
